3f1793a6f3ceff0b9351378ba7b8134120df52a8
Mathieu Audibert <mathieu.audibert27@gmail.com>
2024-04-09 14:26:43 +0200

    composer init + connexion bdd - mathieu

---
+++ src/bdd.php
@@ -0,0 +1,15 @@
+<?php
+
+$dsn = "pgsql:host=dpg-coagp3779t8c73ehtqjg-a.frankfurt-postgres.render.com;dbname=tradezusichen;port=5432;";
+
+$username = "tradezusichen_user";
+$password = "TfrKbwVdysyKri88siTQi7bgJN3Nud1j";
+
+
+try {
+    $pdo = new PDO($dsn, $username, $password);
+    echo 'Connecté a la BDD!';
+} catch (PDOException $e) {
+    echo "Erreur de connexion : " . $e->getMessage();
+}
+?>
